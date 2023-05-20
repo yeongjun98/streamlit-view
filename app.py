f8289c1a31bcb4e79996dc1c2d809dbf7afe8460
yeongjun98 <66932565+yeongjun98@users.noreply.github.com>
2023-05-20 15:04:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 view = [100, 150, 30]
-st.write('# Youtube view')
-st.write('## raw')
+st.write('# 계절학기 교과목 추천')
+st.write('## 수강 가능 교과목')
 view
-st.write('## bar chart')
+st.write('## 수강 후기')
 st.bar_chart(view)
 import pandas as pd
+st.write('## 교과목 세부 정보')
 sview = pd.Series(view)
 sview
