f7e585c2acadf15935478eff238ba56bfacfdec2
yeongjun98 <66932565+yeongjun98@users.noreply.github.com>
2023-05-25 20:47:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
-import numpy as np
-import pandas as pd 
-from sklearn.datasets import load_iris 
-import matplotlib.pyplot as plt
 import streamlit as st
+import datetime
 
+# 페이지: 과목추천
+def page_subject_recommendation():
+    st.title("과목 추천")
+    major = st.text_input("학과를 입력하세요.")
+    name = st.text_input("이름을 입력하세요.")
+    student_id = st.text_input("학번을 입력하세요.")
+    remaining_credits = st.number_input("잔여학점을 입력하세요.", min_value=0)
+    theme = st.selectbox("테마를 선택하세요.", ["밝은 테마", "어두운 테마"])
 
-iris_dataset = load_iris()
+    if st.button("추천 과목 확인"):
+        st.write(f"학과: {major}")
+        st.write(f"이름: {name}")
+        st.write(f"학번: {student_id}")
+        st.write(f"잔여학점: {remaining_credits}")
+        st.write(f"선택한 테마: {theme}")
 
-df= pd.DataFrame(data=iris_dataset.data,columns= iris_dataset.feature_names)
-df.columns= [ col_name.split(' (cm)')[0] for col_name in df.columns] # 컬럼명을 뒤에 cm 제거하였습니다
-df['species']= iris_dataset.target 
+# 페이지: 전남대학교 스케쥴
+def page_university_schedule():
+    st.title("전남대학교 스케쥴")
+    selected_date = st.date_input("날짜를 선택하세요.", datetime.date.today())
 
+    if st.button("스케쥴 확인"):
+        st.write(f"선택한 날짜: {selected_date}")
 
-species_dict = {0 :'setosa', 1 :'versicolor', 2 :'virginica'} 
+# 페이지: 공지사항
+def page_announcements():
+    st.title("공지사항")
+    st.write("현재 공지사항이 없습니다.")
 
-def mapp_species(x):
-  return species_dict[x]
+# 메인 애플리케이션
+def main():
+    # 네비게이션 메뉴 설정
+    pages = {
+        "과목 추천": page_subject_recommendation,
+        "전남대학교 스케쥴": page_university_schedule,
+        "공지사항": page_announcements
+    }
 
+    # 네비게이션 바
+    st.sidebar.title("메뉴")
+    selected_page = st.sidebar.radio("페이지 선택", list(pages.keys()))
 
-df['species'] = df['species'].apply(mapp_species)
-print(df)
+    # 선택한 페이지 실행
+    pages[selected_page]()
 
-st.subheader('this is table')
-st.table(df.head())
+if __name__ == "__main__":
+    main()
 
-st.subheader('this is data frame')
-st.dataframe(df.head())
