8431e8feb1e092818e82ad84286c38a641634b8a
yeongjun98 <66932565+yeongjun98@users.noreply.github.com>
2023-05-25 20:27:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
-import numpy as np
-import pandas as pd 
-from sklearn.datasets import load_iris 
-import matplotlib.pyplot as plt
 import streamlit as st
 
+col1,col2 = st.columns([2,3])
+# 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성합니다.  
 
-iris_dataset = load_iris()
-
-df= pd.DataFrame(data=iris_dataset.data,columns= iris_dataset.feature_names)
-df.columns= [ col_name.split(' (cm)')[0] for col_name in df.columns] # 컬럼명을 뒤에 cm 제거하였습니다
-df['species']= iris_dataset.target 
-
-
-species_dict = {0 :'setosa', 1 :'versicolor', 2 :'virginica'} 
-
-def mapp_species(x):
-  return species_dict[x]
+with col1 :
+  # column 1 에 담을 내용
+  st.title('here is column1')
+with col2 :
+  # column 2 에 담을 내용
+  st.title('here is column2')
+  st.checkbox('this is checkbox1 in col2 ')
 
 
-df['species'] = df['species'].apply(mapp_species)
-print(df)
-
-# 사이드바에 select box를 활용하여 종을 선택한 다음 그에 해당하는 행만 추출하여 데이터프레임을 만들고자합니다.
-st.sidebar.title('Iris Species🌸')
-
-# select_species 변수에 사용자가 선택한 값이 지정됩니다
-select_species = st.sidebar.selectbox(
-    '확인하고 싶은 종을 선택하세요',
-    ['setosa','versicolor','virginica']
-)
-# 원래 dataframe으로 부터 꽃의 종류가 선택한 종류들만 필터링 되어서 나오게 일시적인 dataframe을 생성합니다
-tmp_df = df[df['species']== select_species]
-# 선택한 종의 맨 처음 5행을 보여줍니다 
-st.table(tmp_df.head())
+# with 구문 말고 다르게 사용 가능 
+col1.subheader(' i am column1  subheader !! ')
+col2.checkbox('this is checkbox2 in col2 ') 
+#=>위에 with col2: 안의 내용과 같은 기능을합니다
