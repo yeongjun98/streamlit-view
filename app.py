38b7b67114deffda10ff163c04c3b0ff177f2ffd
yeongjun98 <66932565+yeongjun98@users.noreply.github.com>
2023-05-26 10:49:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -268,14 +268,12 @@ def page_university_schedule():
                 {'start': '2023-12-15', 'end': '2024-01-10', 'name': '제2학기 교수수업개선서(CQI) 입력'}, 
                 {'start': '2023-12-15', 'end': '2024-01-03', 'name': '제2학기 최종 수업평가'}])
         
-        name = []
-        for i in range(116):
+        for i in range(87):
             if str(df2['start'][i]) == str(selected_date) :
-                name.append()
-        st.write(f"{selected_date}, 일정입니다.")
-        st.write(f"{df2['name'][i]}")
-                
-
+                st.write(f"{selected_date}, 일정입니다.")
+                st.write(f"{df2['name'][i]}")
+            
+            
                 
         
 
